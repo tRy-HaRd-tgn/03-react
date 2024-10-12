9361f449b5ee6da03335cb772d13d61979a59d0a
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-10-12 16:46:20 +0300

    alxam

--- src/components/header/MyHeader.jsx
+++ src/components/header/MyHeader.jsx
@@ -1,17 +1,21 @@
 import classes from "./header.module.css";
 import logo from "../../img/converted.svg";
-import avatar from '../../img/avatars/avatar-1.jpg'
+import avatar from "../../img/avatars/avatar-1.jpg";
 
 import clipper from "../../img/clipper.svg";
 import MyProfile from "../profile/MyProfile";
 import MyButton from "../ui/MyButton";
 export default function MyHeader(props) {
+  // залупай
+  // шайлушай
   return (
     <header className={classes.header}>
       <img src={logo} alt="" style={{ marginLeft: "32px" }} />
-      <MyButton  color='green' size='normal'>Играть</MyButton>
+      <MyButton color="green" size="normal">
+        Играть
+      </MyButton>
       <div className={classes.user}>
-        <MyProfile name={'Paromovevg'} avatar={avatar} rank={1230} />
+        <MyProfile name={"Paromovevg"} avatar={avatar} rank={1230} />
         <img src={clipper} alt="" />
       </div>
     </header>
